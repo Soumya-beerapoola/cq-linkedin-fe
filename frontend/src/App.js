fd4ff2ee0a3f621f872d49df71d4960ae8fd2f5f
Soumya <soumya@closequest.com>
2023-07-07 11:25:51 +0530

    fe setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import{ LinkedInApi, NodeServer } from './config';
+import { LinkedInApi, NodeServer } from './config';
 import axios from 'axios';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import linkedInLoginImage from './linkedin-login-images/Retina/Sign-In-Small---Default.png';
+import Home from './Home';
 
 export default class App extends React.Component {
   initialState = {
-    user: {},
+    user: { email: {}},
     loggedIn: false
   };
 
@@ -14,13 +16,20 @@ export default class App extends React.Component {
     this.state = this.initialState;
   }
 
-  componentDidMount = () => {
-    if (window.opener && window.opener !== window) {
-      const code = this.getCodeFromWindowURL(window.location.href);
-      window.opener.postMessage({'type': 'code', 'code': code}, '*')
-      window.close();
-    }
-      window.addEventListener('message', this.handlePostMessage);
+  getUserCredentials = code => {
+    const url = `${NodeServer.baseURL}${NodeServer.getUserCredentials}?code=${code}`
+    debugger
+    axios
+      .get(url)
+      .then(res => {
+        const { user } = res.data;
+        this.setState({
+          user,
+          loggedIn: true
+        })
+      }).catch((e) => {
+        console.log(e)
+      })
   };
 
   handlePostMessage = event => {
@@ -28,22 +37,21 @@ export default class App extends React.Component {
       const { code } = event.data;
       this.getUserCredentials(code);
     }
-  };
+  }
 
-  getCodeFromWindowURL = url => {
-    const popupWindowURL = new URL(url);
-    return popupWindowURL.searchParams.get("code");
-  };
+  componentDidMount() {
+    window.addEventListener('message', this.handlePostMessage);
+  }
 
   showPopup = () => {
     const { clientId, redirectUrl, oauthUrl, scope, state } = LinkedInApi;
-    const oauthUrl = `${oauthUrl}&client_id=${clientId}&scope=${scope}&state=${state}&redirect_uri=${redirectUrl}`;
+    const oauthUrl2 = `${oauthUrl}&client_id=${clientId}&scope=${scope}&state=${state}&redirect_uri=${redirectUrl}`;
     const width = 450,
       height = 730,
       left = window.screen.width / 2 - width / 2,
       top = window.screen.height / 2 - height / 2;
     window.open(
-      oauthUrl,
+      oauthUrl2,
       'Linkedin',
       'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' +
       width +
@@ -56,37 +64,27 @@ export default class App extends React.Component {
     );
   };
 
-  getUserCredentials = code => {
-    axios
-      .get(`${NodeServer.baseURL} + ${NodeServer.getUserCredentials}?code=${code}`)
-      .then(res => {
-        const user = res.data;
-        this.setState({
-          user,
-          loaded: true
-        })
-        // Do something with user
-      });
-  };
-
   render() {
     const { loggedIn, user } = this.state;
     const contentWhenLoggedIn = (
       <>
+        <div>after login</div>
         <img src={user.profileImageURL} alt="Profile image" />
         <h3>{`${user.firstName} ${user.lastName}`}</h3>
-        <h3>{user.email}</h3>
+        <h3>{user.email.emailAddress}</h3>
       </>
     );
     const contentWhenLoggedOut = (
-        <>
-          <h2>Sign in with LinkedIn</h2>
-          <img src={linkedInLoginImage} alt="Sign in with LinkedIn"onClick={this.showPopup} />
-        </>
+      <>
+        <h2>Sign in with LinkedIn - 1</h2>
+        <img src={linkedInLoginImage} alt="Sign in with LinkedIn" onClick={this.showPopup} />
+      </>
     );
     return (
       <div>
+
         {loggedIn && user !== {} ? contentWhenLoggedIn : contentWhenLoggedOut}
+
       </div>
     )
   };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {  RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/linkedin',
+    element: <Home />
+  }
+]);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
