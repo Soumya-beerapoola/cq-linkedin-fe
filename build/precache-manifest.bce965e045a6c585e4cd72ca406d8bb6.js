9979d36410fe36a23d5539bd6c303fcade858764
Soumya <soumya@closequest.com>
2023-07-07 14:34:40 +0530

    after build

---
+++ build/precache-manifest.bce965e045a6c585e4cd72ca406d8bb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4692fbc11447ec0a6211d7f076f6f04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21375b54e24d57fbe0b0",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "2f4902084f67de84bcca",
+    "url": "/static/js/2.a55b51e1.chunk.js"
+  },
+  {
+    "revision": "73ebcd68d984e1011d4762135b44c65a",
+    "url": "/static/js/2.a55b51e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21375b54e24d57fbe0b0",
+    "url": "/static/js/main.f6d6bc80.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  }
+]);
