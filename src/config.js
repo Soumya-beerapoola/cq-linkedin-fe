ff9465b097deed75680dd626b0c2a47fdf7c860c
Soumya <soumya@closequest.com>
2023-07-07 12:27:59 +0530

    update urls

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 export const LinkedInApi = {
     clientId: '78x7t1w2judka1',
-    redirectUrl: 'http://localhost:8080/linkedin',
+    redirectUrl: 'https://cq-linkedinfe.onrender.com/linkedin',
     oauthUrl: 'https://www.linkedin.com/oauth/v2/authorization?response_type=code',
     scope: 'r_liteprofile%20r_emailaddress',
     state: '123456'
 };
 
 export const NodeServer = {
-    baseURL: 'http://localhost:3003',
+    baseURL: 'https://cq-linkedin-service.onrender.com',
     getUserCredentials: '/getUserCredentials'
 };
