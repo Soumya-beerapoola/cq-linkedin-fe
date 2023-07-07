dbed5e955f1eb5b9a7a05b1c8941753de0fcc2b3
Soumya <soumya@closequest.com>
2023-07-07 15:48:38 +0530

    add render url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const LinkedInApi = {
     clientId: '78x7t1w2judka1',
-    redirectUrl: 'poc.closequest.com/linkedin',
+    redirectUrl: 'https://cq-linkedinfe.onrender.com/linkedin',
     oauthUrl: 'https://www.linkedin.com/oauth/v2/authorization?response_type=code',
     scope: 'r_liteprofile%20r_emailaddress',
     state: '123456'
